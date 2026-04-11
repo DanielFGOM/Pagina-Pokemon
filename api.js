d245182c87952e90ddcf86a3ed800099de2f850c
Daniel Gonzalez <danielfranciscogom@gmail.com>
2026-04-11 17:07:33 -0600

    Cambios dev

---
+++ api.js
@@ -0,0 +1,21 @@
+        fetch ('https://raw.githubusercontent.com/fanzeyi/pokemon.json/refs/heads/master/pokedex.json')
+            .then( response => response.json() )
+            .then( data => mostrarData(data) )
+
+        const mostrarData = (data) => {
+            let body  = ""
+            for (var i = 0; i < data.length; i++) {      
+               body+=`<tr class="text-center" ><td>${data[i].id}</td><td>${data[i].name.english }</td><td>${data[i].type}</td><td>${data[i].base.HP}</td><td>${data[i].base.Attack}</td><td>${data[i].base.Defense}</td><td>${data[i].base.Speed}</td></tr>`
+            }
+            document.getElementById('data').innerHTML = body
+        }
+        const signUpButton = document.getElementById('signUp');
+        const signInButton = document.getElementById('signIn');
+        const container = document.getElementById('container');
+        signUpButton.addEventListener('click', () => {
+       container.classList.add("right-panel-active");
+       });
+       signInButton.addEventListener('click', () => {
+       container.classList.remove("right-panel-active");
+       });        
+
